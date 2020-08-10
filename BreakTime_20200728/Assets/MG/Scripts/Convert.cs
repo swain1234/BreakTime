0be4a6c0217f62ad769f3f27c2a3d14f10be7b08
infe <kmko0914@gmail.com>
2020-08-10 20:45:59 +0900

    feat: FlipBook추가
    텍스쳐캡쳐에 맞춰 코드 수정중

---
+++ BreakTime_20200728/Assets/MG/Scripts/Convert.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Convert : MonoBehaviour
+{
+    Texture2D ConvertSpriteToTexture(Sprite sprite)
+    {
+        try
+        {
+            if (sprite.rect.width != sprite.texture.width)
+            {
+                Texture2D newText = new Texture2D((int)sprite.rect.width, (int)sprite.rect.height);
+                Color[] colors = newText.GetPixels();
+                Color[] newColors = sprite.texture.GetPixels((int)System.Math.Ceiling(sprite.textureRect.x),
+                                                             (int)System.Math.Ceiling(sprite.textureRect.y),
+                                                             (int)System.Math.Ceiling(sprite.textureRect.width),
+                                                             (int)System.Math.Ceiling(sprite.textureRect.height));
+                Debug.Log(colors.Length + "_" + newColors.Length);
+                newText.SetPixels(newColors);
+                newText.Apply();
+                return newText;
+            }
+            else
+                return sprite.texture;
+        }
+        catch
+        {
+            return sprite.texture;
+        }
+    }
+
+    public static Texture2D textureFromSprite(Sprite sprite)
+    {
+        if (sprite.rect.width != sprite.texture.width)
+        {
+            Texture2D newText = new Texture2D((int)sprite.rect.width, (int)sprite.rect.height);
+            Color[] newColors = sprite.texture.GetPixels((int)sprite.textureRect.x,
+                                                         (int)sprite.textureRect.y,
+                                                         (int)sprite.textureRect.width,
+                                                         (int)sprite.textureRect.height);
+            newText.SetPixels(newColors);
+            newText.Apply();
+            return newText;
+        }
+        else
+            return sprite.texture;
+    }
+}
