5bef188fedd0cebc9d48a44c87e3f6db993a58be
infe <kmko0914@gmail.com>
2020-08-10 19:17:27 +0900

    feat: TextureCapTure추가
    mainCamera에서 Texture캡쳐가능

---
+++ BreakTime_20200728/Assets/MG/Scripts/TextureManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextureManager : MonoBehaviour
+{
+    private int resWidth = 1600;
+    private int resHeight = 1200;
+
+    [SerializeField] RawImage rawImage;
+
+    void Awake()
+    {
+        resWidth = Screen.width;
+        resHeight = Screen.height;
+    }
+
+    void LateUpdate()
+    {
+        if(Input.GetKeyDown("k"))
+        {
+            RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
+            GetComponent<Camera>().targetTexture = rt;
+            GetComponent<Camera>().Render();
+            rawImage.texture = rt;
+            RenderTexture.active = rt;
+            GetComponent<Camera>().targetTexture = null;
+            RenderTexture.active = null; // JC: added to avoid errors
+        }
+    }
+}
