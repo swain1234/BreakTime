293efdd8c63243fc51332349453354de007733db
ajang369 <37576725+ajang369@users.noreply.github.com>
2020-08-14 05:45:39 +0900

    fix: 애니메이션 작업 커밋 오류로 다시 커밋

---
+++ BreakTime_20200728/Assets/HJ/Scripts/Player/P2_Move.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine.Unity;
+
+public class P2_Move : MonoBehaviour
+{
+    public float maxSpeed;
+    public float stopSpeed;
+    Rigidbody2D rigid;
+    Animator animator;
+    SkeletonAnimation skeleton;
+    bool faceLeft = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        skeleton = GetComponentInChildren<SkeletonAnimation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 키보드에서 손을 땠을 때 미끄러지면서 멈춤
+        if (Input.GetButtonUp("Move2"))
+        {
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * stopSpeed, rigid.velocity.y);
+        }
+
+        // 방향 전환 Default = left
+        if (Input.GetAxisRaw("Move2") < 0 && !faceLeft)
+        {
+            // left
+            skeleton.initialFlipX = false;
+            Flip();
+        }
+        else if (Input.GetAxisRaw("Move2") > 0 && faceLeft)
+        {
+            // right
+            skeleton.initialFlipX = true;
+            Flip();
+        }
+
+        // 애니메이션
+        // 점프 중에는 isMove - false
+        if (animator.GetBool("isJump"))
+        {
+            animator.SetBool("isMove", false);
+
+        }
+        // 이동 중
+        else
+        {
+            // 이동속도가 0.1 이하일 경우 isMove - false
+            if (Mathf.Abs(rigid.velocity.x) < 0.3)
+            {
+                animator.SetBool("isMove", false);
+
+            }
+            // 그 외에는 isMove - true
+            else
+            {
+                animator.SetBool("isMove", true);
+
+            }
+
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        // 이동
+        float move = Input.GetAxisRaw("Move2");
+        rigid.AddForce(Vector2.right * move, ForceMode2D.Impulse);
+
+        // 최대 속도 조절
+        if (rigid.velocity.x > maxSpeed)
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        else if (rigid.velocity.x < maxSpeed * (-1))
+        {
+            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+        }
+    }
+
+    void Flip()
+    {
+        faceLeft = !faceLeft;
+        Vector2 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
+
---
+++ BreakTime_20200728/Assets/HJ/Scripts/Player/PlayerJump.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    public float jumpPower = 5f;
+    public int currentCount = 0;
+    public int jumpCount = 1;
+    public float rayDistance;
+    public bool isJump = false;
+    // ray 시작위치 조절
+    public float rayPosition = 0.3f;
+
+    Animator animator;
+    Rigidbody2D rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckGround();
+    }
+
+    // 레이캐스트
+    void CheckGround()
+    {
+        Vector2 rayPos = new Vector3(transform.position.x, transform.position.y - rayPosition);
+        int layerMask = (1 << LayerMask.NameToLayer("Ground")) + (1 << LayerMask.NameToLayer("Doodle"));
+        RaycastHit2D hit = Physics2D.Raycast(rayPos , Vector3.down, rayDistance, layerMask);
+        Debug.DrawRay(rayPos, Vector3.down * rayDistance, Color.red);
+
+        if(hit.collider != null)
+        {
+            if(hit.transform.CompareTag("Ground") || hit.transform.CompareTag("Doodle"))
+            {
+                //Debug.Log("Ground");
+                animator.SetBool("isJump", false);
+                animator.SetBool("isJumpDown", false);
+                animator.SetBool("isJumpUp", false);
+                currentCount = 0;
+                return;
+            }
+        }
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if(gameObject.tag == "Player1")
+        {
+            if(Input.GetKeyDown(KeyCode.UpArrow) && !animator.GetBool("isJump"))
+            {
+                if(currentCount < jumpCount)
+                {
+                    isJump = true;
+                    jumpCount++;
+                    animator.SetBool("isJump", true);
+                    animator.SetBool("isJumpUp", true);
+                }
+            }
+        }
+        else if(gameObject.tag == "Player2")
+        {
+            if(Input.GetKeyDown(KeyCode.W) && !animator.GetBool("isJump"))
+            {
+                if(currentCount < jumpCount)
+                {
+                    isJump = true;
+                    jumpCount++;
+                    animator.SetBool("isJump", true);
+                    animator.SetBool("isJumpUp", true);
+                }
+            }
+        }
+
+        Jump();
+        
+        // JumpDown
+        if (rigid.velocity.y < -0.01)
+        {
+            animator.SetBool("isJumpDown", true);
+            //animator.SetBool("isJump", true);
+        }
+
+    }
+
+    void Jump()
+    {
+        if (!isJump)
+            return;
+        rigid.velocity = Vector2.zero;
+
+        Vector2 jumpVelocity = new Vector2(0f, jumpPower);
+        rigid.AddForce(jumpVelocity, ForceMode2D.Impulse);
+        Debug.Log("Jump");
+        isJump = false;
+    }
+}
