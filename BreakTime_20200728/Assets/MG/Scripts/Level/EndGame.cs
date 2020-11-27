fc54654325fd059965bd075da240069132138e98
Mingi <kmko0914@gmail.com>
2020-11-27 16:21:09 +0900

    real final

---
+++ BreakTime_20200728/Assets/MG/Scripts/Level/EndGame.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    [SerializeField] GameObject Canvas;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!Canvas.gameObject.activeSelf)
+                Canvas.gameObject.SetActive(true);
+            else
+                Canvas.gameObject.SetActive(false);
+        }
+    }
+
+    public void YesButton()
+    {
+        Application.Quit();
+    }
+
+    public void NoButton()
+    {
+        Canvas.gameObject.SetActive(false);
+    }
+}
